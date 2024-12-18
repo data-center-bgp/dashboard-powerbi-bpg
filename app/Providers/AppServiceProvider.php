ea66b25f7b434fd45967d1c0231113fa06ca774f
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-12-18 09:04:03 +0800

    fix model issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::unguard();
+
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
